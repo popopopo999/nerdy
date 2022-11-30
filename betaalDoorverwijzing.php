33347ee11b8e5419509241c3f778ca96ef79a179
Bastiaan <s1067341@student.windesheim.nl>
2022-11-30 13:38:56 +0100

    Controleert of account maken is aangevinkt en verwijst de gebruiker vervolgens door naar signup op de Ideal pagina

--- betaalDoorverwijzing.php
+++ betaalDoorverwijzing.php
@@ -1,2 +1,9 @@
 <?php
-print("test");
+$url = $_SERVER['REQUEST_URI'];
+$urlQuery = substr($url, strpos($url, "?"));
+
+if(isset($_GET["Account_maken"])){
+    header('Location: Signup.php' . $urlQuery);
+}else{
+    header('Location: https://www.ideal.nl/demo/qr/?app=ideal');
+}
