5e3adecff9f44f0a494125dd6417912f0201ec88
BasBierhof <s1067341@student.windesheim.nl>
2022-12-13 21:54:25 +0100

    bestelling plaatsen toegevoegd. TODO: In stock verlagen.

--- betaalDoorverwijzing.php
+++ betaalDoorverwijzing.php
@@ -1,6 +1,10 @@
 <?php
+include __DIR__ . "/header.php";
+
 if(isset($_SESSION["Gebruikersnaam"])){
+    insertBestelling($databaseConnection, $_POST);
     header('Location: https://www.ideal.nl/demo/qr/?app=ideal');
+    exit();
 }
 
 if(isset($_POST["Account_maken"])){
@@ -8,5 +12,7 @@ if(isset($_POST["Account_maken"])){
     $_SESSION["Account_maken"] = $_POST;
     header('Location: Signup.php');
 }else{
+    insertBestelling($databaseConnection, $_POST);
     header('Location: https://www.ideal.nl/demo/qr/?app=ideal');
+    exit();
 }
